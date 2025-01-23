3bc1c5c57946e43d4c056e6d8dc3ce57fd179fb9
Arlo Watts <arlowatts1@gmail.com>
2025-01-23 13:03:44 -0800

    Add message to link share feature

--- src/creator.js
+++ src/creator.js
@@ -2,7 +2,7 @@ import { tileDisplay } from "./tileDisplay.js";
 import { Puzzle } from "./puzzle.js";
 
 export class Creator {
-    #layout = ["CLUE", [], "SOLUTION", [], "REVEALED LETTERS", [], [["Play!"]]];
+    #layout = ["CLUE", [], "SOLUTION", [], "REVEALED LETTERS", [], [["Share"]], [["Play"]]];
 
     #cssClasses = [[], [], ["tile", "button"]];
 
@@ -12,6 +12,9 @@ export class Creator {
 
     constructor(paramNames) {
         this.#paramNames = paramNames;
+
+        if (this.#paramNames.length !== 3)
+            throw new RangeError("Incorrect number of parameters");
     }
 
     initializeDisplay(wrapper) {
@@ -22,11 +25,21 @@ export class Creator {
     }
 
     initializeEventListeners() {
-        this.#refs[2][0].addEventListener("click", () => { this.submit(); });
+        this.#refs[2][0].addEventListener("click", (e) => { this.share(e); });
+        this.#refs[2][1].addEventListener("click", () => { this.submit(); });
         addEventListener("keydown", (e) => { if (e.key === "Enter") this.submit(); });
     }
 
+    share(e) {
+        navigator.clipboard.writeText("\"" + this.#refs[1][0].children[0].value.toUpperCase() + "\"\n" + window.location.origin + window.location.pathname + "?" + this.getSearchParams().toString());
+        e.target.textContent = "Copied!";
+    }
+
     submit() {
+        window.location.search = this.getSearchParams();
+    }
+
+    getSearchParams() {
         const values = this.#refs[1].slice(0, 3).map((x) => x.children[0].value);
 
         const puzzle = new Puzzle(values[0], values[1], values[2]);
@@ -37,6 +50,6 @@ export class Creator {
         for (let i = 0; i < this.#paramNames.length; i++)
             searchParams.set(this.#paramNames[i], encodedPuzzle[i]);
 
-        window.location.search = searchParams;
+        return searchParams;
     }
 }
