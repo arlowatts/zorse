973334225cb3370e363734d52f0707fa449f0253
Arlo Watts <arlowatts1@gmail.com>
2025-02-05 15:56:32 -0800

    Fix css class on tutorial page

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,2 +1,2 @@
 addEventListener("keydown", (e) => { if (e.key === "Enter") window.history.back(); });
-document.getElementById("backbutton").addEventListener("click", () => { window.history.back(); });
+document.getElementById("back-button").addEventListener("click", () => { window.history.back(); });
