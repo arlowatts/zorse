51ae2acd690604cb2389e26221aa1ff0c1eae82b
Arlo Watts <arlowatts1@gmail.com>
2025-02-06 10:50:57 -0800

    Embed tutorial script into html

--- src/tutorial.js
+++
@@ -1,2 +0,0 @@
-addEventListener("keydown", (e) => { if (e.key === "Enter") window.history.back(); });
-document.getElementById("back-button").addEventListener("click", () => { window.history.back(); });
