dd5b2963b54cd8268684df72f6f9c312b34b3a26
Arlo Watts <arlowatts1@gmail.com>
2025-02-06 11:52:59 -0800

    Rewrite and comment creator code

--- src/creator.js
+++ src/creator.js
@@ -2,45 +2,37 @@ import * as puzzle from "./puzzle.js";
 
 export const elements = { styles: ["wrapper"], children: [
     { },
-    { children: [
-        "CLUE",
-        { tag: "input", styles: ["border"] },
-    ] },
-    { children: [
-        "SOLUTION",
-        { tag: "input", styles: ["border"] },
-    ] },
-    { children: [
-        "REVEALED LETTERS",
-        { tag: "input", styles: ["border"] },
-    ] },
-    { children: [
-        { styles: ["border", "tile", "button"], children: ["Share"] },
-    ] },
-    { children: [
-        { styles: ["border", "tile", "button"], children: ["Play"] },
-    ] },
+    { children: ["CLUE", { tag: "input", styles: ["border"] }] },
+    { children: ["SOLUTION", { tag: "input", styles: ["border"] }] },
+    { children: ["REVEALED LETTERS", { tag: "input", styles: ["border"] }] },
+    { children: [{ styles: ["border", "tile", "button"], children: ["Share"] }] },
+    { children: [{ styles: ["border", "tile", "button"], children: ["Play"] }] },
     { },
 ] };
 
-const clue = elements.children[1].children[1];
-const solution = elements.children[2].children[1];
-const letters = elements.children[3].children[1];
-
-const shareButton = elements.children[4].children[0];
-const playButton = elements.children[5].children[0];
+const wrappers = {
+    clue: elements.children[1].children[1],
+    solution: elements.children[2].children[1],
+    letters: elements.children[3].children[1],
+    share: elements.children[4].children[0],
+    play: elements.children[5].children[0],
+};
 
 export function initializeEventListeners() {
-    puzzle.loadPuzzle([clue.ref, solution.ref, letters.ref]);
+    // set the puzzle parameters
+    puzzle.loadPuzzle([wrappers.clue.ref, wrappers.solution.ref, wrappers.letters.ref]);
 
-    shareButton.ref.addEventListener("click", () => {
+    // share the puzzle URL when the share button is clicked
+    wrappers.share.ref.addEventListener("click", () => {
         navigator.share({ text: puzzle.shareURL() });
     });
 
-    playButton.ref.addEventListener("click", () => {
+    // navigate to the puzzle page when the play button is clicked
+    wrappers.play.ref.addEventListener("click", () => {
         location = puzzle.getURL();
     });
 
+    // navigate to the puzzle page when the enter key is pressed
     addEventListener("keydown", (e) => {
         if (e.key === "Enter")
             location = puzzle.getURL();
